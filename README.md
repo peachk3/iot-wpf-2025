404c6a162169bfa01fb675be18fb4109f749f647
DoGyoung Park <pdk1896@kakao.com>
2025-05-19 17:16:27 +0900

    8일차

--- README.md
+++ README.md
@@ -404,4 +404,70 @@ IoT 개발자 WPF 학습 리포지토리
     https://github.com/user-attachments/assets/0bdefea3-877f-4b8d-9c49-3334aba746cd
 
 
+### 스마트홈 연동 모니터링앱
 
+<img src="./image/WPF0022.jpg" width="650">
+
+- 전면부
+
+<img src="./image/WPF0023.jpg" width="650">
+
+- 후면부
+
+- [개발링크](https://github.com/hugoMGSung/hungout-with-arduino/tree/main/SmartHomeDIY)
+
+1. Arduino + Raspbery Pi 스마트홈 기제작
+
+#### MQTT
+
+<img src="./image/wpf0026.png" width="600">
+
+- Message Queueing Telemetry Transport : 기계 간 통신용 경량 메시징 프로토콜
+- Publish / Subscribe 용어로 사용
+    - Publish(출간) : 메시지 만들어서 전달
+    - Subscribe(구독) : 필요한 메시지를 수신 받아서 사용
+- Server(MQTT 브로커)/Client 프로그램으로 동작
+- 데이터는 휘발성 : 받는 사람이 없으면 데이터는 사라짐. DB에 저장하는 구성을 해줘야 함
+
+- MQTT를 대체할 수 있는 유사한 기능을 하는 기술
+    - `Redis`, `Apache Kafka`, *RabbitMQ*, ZeroMQ, Socket통신 직접 개발
+
+#### MQTT 시물레이션
+1. MQTT 브로커 설치
+    - https://mosquitto.org/
+    - mosquitto-2.0.21a-install-windows-x64.exe 설치
+    - 설치 후 서비스에서 서비스 중지
+
+2. 모스키토 설정파일 수정
+    - mosquitto.conf 문서에디터 관리자모드로 오픈
+    - #listener -> listener 1883으로 변경
+    - #allow_anonymous false -> allow_anonymous true
+    - 파일 저장 후, 서비스 재시작
+3. Windows 보안
+    - 방화벽 및 네트워크 보호 > 고급 설정
+    - 인바운드 규칙 > 새 규칙
+    - 포트 선택 > 다음
+    - TCP 선택, 특정 포트 1883 입력
+4. MQTT Explorer 설치
+    - new Connection 생성, Host 127.0.0.1, Port 1883 저장
+    - CONNECT
+5. VS Code에서 [MQTTPub.py](./day08/Pythons/MQTTPub.py) 파일 생성
+
+    
+
+### 스마트홈 프로젝트 시작
+1. 화면 UI 변경
+2. NuGet 패키지
+    - CommunityToolkit.Mvvm 설치
+3. Models, Views, ViewModels 폴더 생성
+4. MainWindow 바인딩 처리
+5. MainViewModel에서 바인딩 속성 초기화
+
+    <img src="./image/wpf0025.png" width="650">
+
+
+## 9일차
+
+### 스마트홈 연동 모니터링앱 (계속)
+
+#### MQTT 시뮬레이션 (계속)
---
+++ day08/Day08Study/WpfSmartHomeApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,103 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Security.Policy;
+using System.Windows;
+
+namespace WpfSmartHomeApp.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        private double _homeTemp;
+        private double _homeHumid;
+        private string _detectResult;
+        private bool _isDetectOn;
+        private string _rainResult;
+        private bool _isRainOn;
+        private string _airConResult;
+        private bool _isAirConOn;
+        private string _lightResult;
+        private bool _isLightOn;
+
+        // 온도 속성
+        public double HomeTemp
+        {
+            get => _homeTemp;
+            set => SetProperty(ref _homeTemp, value);
+        }
+
+        // 습도 속성
+        public double HomeHumid
+        {
+            get => _homeHumid;
+            set => SetProperty(ref _homeHumid, value);
+        }
+        
+        // 사람인지
+        public string DetectResult
+        {
+            get => _detectResult;
+            set => SetProperty(ref _detectResult, value);
+        }
+
+        // 사람인지 여부
+        public bool IsDetectOn
+        {
+            get => _isDetectOn;
+            set => SetProperty(ref _isDetectOn, value);
+        }
+
+        public string RainResult
+        {
+            get => _rainResult;
+            set => SetProperty(ref _rainResult, value);
+        }
+
+        public bool IsRainOn
+        {
+            get => _isRainOn;
+            set => SetProperty(ref _isRainOn, value);
+        }
+
+        public string AirConResult
+        {
+            get => _airConResult;
+            set => SetProperty(ref _airConResult, value);
+        }
+
+        public bool IsAirConOn
+        {
+            get => _isAirConOn;
+            set => SetProperty(ref _isAirConOn, value);
+        }
+
+        public string LightResult
+        {
+            get => _lightResult;
+            set => SetProperty(ref _lightResult, value);
+        }
+
+        public bool IsLightOn
+        {
+            get => _isLightOn;
+            set => SetProperty(ref _isLightOn, value);
+        }
+
+        // LoadedCommand 에서 앞에 On이 붙고 Command는 삭제
+        [RelayCommand]
+
+        public void OnLoaded()
+        {
+            HomeTemp = 27;
+            HomeHumid = 43.2;
+
+            DetectResult = "Decteted Human!";
+            IsDetectOn = false;
+            RainResult = "Raining";
+            IsRainOn = true;
+            AirConResult = "AirCon On";
+            IsAirConOn = true;
+            LightResult = "Light On";
+            IsLightOn = true;
+        }
+    }
+}
